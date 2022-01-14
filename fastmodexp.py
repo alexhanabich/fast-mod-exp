ab7b7ced8edd3333f88a474081b865ad53ca55cd
Alex Kang <alexhanabich@gmail.com>
2022-01-14 12:06:15 +0900

    Update fastmodexp.py

--- fastmodexp.py
+++ fastmodexp.py
@@ -8,24 +8,33 @@
 # so, g^a can be expressed as product of g^(2^i) for i: 0 to (num_bit -1) where i != 0
 
 # let x_i be g^(2^i)
-# then, x_i+1 = g^(2^(i+1)) = g^(2^i * 2) = (g^(2^i)) ^ 2 = (x_i)^2
+# then, x_(i+1) = g^(2^(i+1)) = g^(2^i * 2) = (g^(2^i)) ^ 2 = (x_i)^2
 # therefore, to proceed to the next term, we square the prev term
 # let y_i be x_i mod p
+
 # Modular Multiplication: (A * B) mod C = (A mod C * B mod C) mod C
-# then y_i+1 = x_i+1 mod p = (x_i * x_i) mod p = (x_i mod p)^2 mod p = (y_i)^2 mod p
-# then g^a mod p can be written as a sequence:
+
+# then y_(i+1) = x_(i+1) mod p = (x_i * x_i) mod p = (x_i mod p)^2 mod p = (y_i)^2 mod p (line 15)
+# then g^a mod p can be written as a (product of the sequence) mod p:
 #   y_0 = x_0 mod p = g^(2^0) mod p = g mod p
-#   y_i+1 = (y_i)^2 mod p for i: 0 to (num_bit - 1)
+#   y_(i+1) = (y_i)^2 mod p for i: 0 to (num_bit-1)
 
 
 # g = base, a = exponenet, p = modulus, y = y_i from the sequence above
 def mod_exp(g, a, p):
+    # r is a product of all y values (y_0 ....... y_(numbit-1)
     r = 1
+    # y_0 is g mod p (line 19)
     y = g % p
     while a > 0:
+        # when the leftmost bit is set to 1 (line 7)
         if a & 1 == 1:
+            # product of y_i and y_(i+1)
+            # mod p is necessary bcause the product can easily exceed p
             r = (r * y) % p
+        # move y_i to y_(i+1) (line 20)
         y = y**2 % p
+        # move to the next bit in a
         a >>= 1
     return r
     
