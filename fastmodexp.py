37a4ea2bef9b0cdc5e68cdc081d474800c6969ef
Alex Kang <alexhanabich@gmail.com>
2022-01-14 10:57:39 +0900

    Update fastmodexp.py

--- fastmodexp.py
+++ fastmodexp.py
@@ -2,18 +2,20 @@
 # let i be the position of bit from the right(starting from 0)
 # let b_i be the bit of the binary representation of a
 # then g^a can be expressed as product of g^(b_i * 2^i); for i: 0 to (num_bit -1)
-# when b_i = 0, g^(b_i * 2^i) = g^0 = 1, so it can be ignored
-# when b_i = 1, g^(b_i * 2^i) = g^(2^i)
-# so, g^a can be expressed as product of g^(2^i) from 0 to (num_bit -1) where i != 0
+# There are two different cases of b_i:
+#   when b_i = 0, g^(b_i * 2^i) = g^0 = 1, so it can be ignored
+#   when b_i = 1, g^(b_i * 2^i) = g^(2^i)
+# so, g^a can be expressed as product of g^(2^i) for i: 0 to (num_bit -1) where i != 0
+
 # let x_i be g^(2^i)
-# then, x_i+1 = g^(2^i+1) = g^(2^i * 2) = (x_i)^2
+# then, x_i+1 = g^(2^(i+1)) = g^(2^i * 2) = (g^(2^i)) ^ 2 = (x_i)^2
 # therefore, to proceed to the next term, we square the prev term
 # let y_i be x_i mod p
 # Modular Multiplication: (A * B) mod C = (A mod C * B mod C) mod C
 # then y_i+1 = x_i+1 mod p = (x_i * x_i) mod p = (x_i mod p)^2 mod p = (y_i)^2 mod p
-# then g^a mod p can be written as a sequence by
+# then g^a mod p can be written as a sequence:
 #   y_0 = x_0 mod p = g^(2^0) mod p = g mod p
-#   y_i+1 = (y_i)^2 mod p for i = 0 to (num_bit - 1)
+#   y_i+1 = (y_i)^2 mod p for i: 0 to (num_bit - 1)
 
 
 # g = base, a = exponenet, p = modulus, y = y_i from the sequence above
